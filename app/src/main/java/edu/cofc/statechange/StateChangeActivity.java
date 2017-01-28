12611f62d430b309ac89028ac8e1081f0372b126
kbriggs <kabriggs991@gmail.com>
2017-01-28 17:10:33 -0500

    Added onSaveInstanceState and onRestoreInstanceState

--- app/src/main/java/edu/cofc/statechange/StateChangeActivity.java
+++ app/src/main/java/edu/cofc/statechange/StateChangeActivity.java
@@ -56,4 +56,20 @@ public class StateChangeActivity extends AppCompatActivity {
 
         Log.i(TAG, "onRestart");
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i(TAG, "onRestoreInstanceState");
+
+    }
+
 }
